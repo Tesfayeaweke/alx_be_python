c84ae9665bdda6491b2c86b67aae130fcfc30562
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-05-21 18:32:40 +0300

    basic operation.py file submitted.

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,17 @@
+# These are varialble assignments.
+number1 = 10
+number2 = 5
+# sum
+sum = number1 + number2
+
+# difference
+subtract = number1 - number2
+
+# Multiply
+multiply = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {subtract}")
+print(f"Multiplication of {number1} and {number2} is {multiply}")
+
 
