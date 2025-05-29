aeecf022047e27107f1e21083dc34a906c220736
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-05-29 21:05:18 +0300

    one correction down.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,30 +1,12 @@
-# task = input("Enter your task: ")
-# Priority = input("Priority (high/medium/low): ").lower()
-# Is_it_time_bound = input("Is it time-bound? (yes/no)").lower()
-
-# match Priority:
-#     case "high":
-#         message = "is a high priority task"
-#     case "medium":
-#         message = "is a medium priority task"
-#     case "low":
-#         message = "is a low priority task"
-
-# if Is_it_time_bound == "yes":
-#      message += " that requires immediate attention today!"
-#      reminder = print(f"Reminder: '{task}' {message}")
-# elif Is_it_time_bound == "no":
-#     message += ". Consider completing it when you have free time."
-#     reminder = print(f"Note: {task} {message}")
 
 
 task = input("Enter your task: ")
 
 Priority = input("Priority (high/medium/low): ").lower()
 
-Is_it_time_bound = input("Is it time-bound? (yes/no): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-# The rest of your code
+
 match Priority:
     case "high":
         message = "is a high priority task"
@@ -33,9 +15,9 @@ match Priority:
     case "low":
         message = "is a low priority task"
 
-if Is_it_time_bound == "yes":
+if time_bound == "yes":
     message += " that requires immediate attention today!"
     print(f"Reminder: '{task}' {message}")
-elif Is_it_time_bound == "no":
+elif time_bound == "no":
     message += ". Consider completing it when you have free time."
     print(f"Note: {task} {message}")
