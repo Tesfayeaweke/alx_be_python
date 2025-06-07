f189329322737bf6bc7c9bf63f43e0985d2da13a
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-07 11:53:00 +0300

    added all the functionalities.

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,40 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item_to_add = input("Enter an item you want to add: ")
+            shopping_list.append(item_to_add)
+            
+        elif choice == '2':
+            # Prompt for and remove an item
+            item_to_remove = input("Enter an item to remove: ")
+            if item_to_remove in shopping_list:
+                shopping_list.remove(item_to_remove)
+            else:
+                print("Item not in the list.")
+            
+        elif choice == '3':
+            # Display the shopping 
+            for item in shopping_list:
+                print(item)
+            
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
