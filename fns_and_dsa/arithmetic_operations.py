8a316b22fca2e0903cf2c796ac602433cdb606fe
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-07 10:56:40 +0300

    third correction made

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,11 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == 'multiply':
           return num1 * num2
-    elif operation == 'divide' and num2 != 0:
-         return num1 / num2
+    elif operation == 'divide':
+         if num2 == 0:
+              return "The second number can't be zero."
+         else:
+              return num1 / num2
     else:
          return "Error, check the values entered." 
     """
