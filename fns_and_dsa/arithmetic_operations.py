864762ceeb999bca5bac0331cbb112acc5dbec52
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-07 10:37:25 +0300

    completed the first attempt

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,14 @@
+# function to perform operation
+def perform_operation(num1,num2,operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide' if num2 != 0:
+            return num1 / num2
+        case _:
+            return "Error, check the values entered." 
 
