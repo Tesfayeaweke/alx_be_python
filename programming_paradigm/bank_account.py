bf0defe65f8892808c6b08c91ec0c845eebf4d51
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-14 19:04:07 +0300

    First submission made.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,1 +1,22 @@
+class BankAccount:
+    def __init__(self,account_balance = 0):
+        self.account_balance = account_balance
 
+    def deposit(self,amount):
+        return self.account_balance + amount
+    
+    def withdraw(self,amount):
+        if amount >= 0 and amount <= self.account_balance:
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Your current deposit is ${self.account_balance}")
+
+    
+
+if __name__ == "__main__":
+    account = BankAccount(50)
+    print(account.display_balance())
+        
+    
