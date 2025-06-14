1b904200ce433edad4e1c7c9ed94ce7788be7390
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-14 21:29:38 +0300

    correction submitted.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,27 +4,27 @@ from simple_calculator import SimpleCalculator
 class TestCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.calculator.add(0,5),5)
-        self.assertEqual(self.calculator.add(-2,-5),-7)
-        self.assertEqual(self.calculator.add(2,-5),-3)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(0,5),5)
+        self.assertEqual(self.calc.add(-2,-5),-7)
+        self.assertEqual(self.calc.add(2,-5),-3)
 
-    def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(0,5),-5)
-        self.assertEqual(self.calculator.subtract(-2,-5),3)
-        self.assertEqual(self.calculator.subtract(2,-5),7)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(0,5),-5)
+        self.assertEqual(self.calc.subtract(-2,-5),3)
+        self.assertEqual(self.calc.subtract(2,-5),7)
 
-    def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(0,5),0)
-        self.assertEqual(self.calculator.multiply(-2,-5),10)
-        self.assertEqual(self.calculator.multiply(2,-5),-10)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(0,5),0)
+        self.assertEqual(self.calc.multiply(-2,-5),10)
+        self.assertEqual(self.calc.multiply(2,-5),-10)
 
-    def test_divide(self):
-        self.assertEqual(self.calculator.divide(0,5),0)
-        self.assertEqual(self.calculator.divide(-2,-5),0.4)
-        self.assertEqual(self.calculator.divide(2,0),None)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(0,5),0)
+        self.assertEqual(self.calc.divide(-2,-5),0.4)
+        self.assertEqual(self.calc.divide(2,0),None)
 
 
 
